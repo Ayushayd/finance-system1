8c15f91e642c7d30ac83418ad86892b9a1e89fc9
Ayush Gupta <ayushe19222@gmail.com>
2025-06-28 23:06:17 +0530

    added .env file to git ignore and added all sensitive data to it

--- src/main/java/com/personalfinance/finance_system/FinanceSystemApplication.java
+++ src/main/java/com/personalfinance/finance_system/FinanceSystemApplication.java
@@ -1,13 +1,20 @@
 package com.personalfinance.finance_system;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FinanceSystemApplication {
-
 	public static void main(String[] args) {
+		// Load .env file
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+
+		// Set each env var into system properties so Spring can pick them up
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(FinanceSystemApplication.class, args);
 	}
-
 }
